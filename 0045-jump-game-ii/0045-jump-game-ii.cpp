d81f1deeeb3863cd9e0c519c52f2db4bb594c6d1
Sumit Kodaan <kodaansumit@gmail.com>
2026-08-04 16:40:37 +0530

    Time: 0 ms (100%), Space: 20.6 MB (77.82%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if(nums.size()==1)
+        return 0;
+
+        int jump=0;
+        int currentend=0;
+        int farthest=0;
+
+        for(int i=0;i<nums.size()-1;i++){
+            farthest=max(farthest,nums[i]+i);
+
+            if(i==currentend){
+                jump++;
+                currentend=farthest;
+            }
+        }
+        return jump;
+    }
+};
