cb6d15e1a7581b3260ab24f8c553f9d5eb6a761f
Sumit Kodaan <kodaansumit@gmail.com>
2026-08-14 16:34:33 +0530

    Time: 0 ms (100%), Space: 8.4 MB (61.93%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+      int count=0;
+      int ans=0;
+
+      for(char ch:s){
+        if(ch=='('){
+            count++;
+
+            ans= max(ans,count);
+        }
+        else if(ch==')'){
+            count--;
+        }
+      } 
+      return ans; 
+    }
+};
